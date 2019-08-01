ecb871b50bbfafae0ceb1126b771f9389a1fd7e3
Zoran <vulezor@gmail.com>
2019-08-01 10:52:20 +0200

    commit2

--- src/app/api/login-api.service.ts
+++ src/app/api/login-api.service.ts
@@ -1,9 +1,6 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+@Injectable()
 export class LoginApiService {
-
-  constructor() { }
+  constructor() {}
 }
