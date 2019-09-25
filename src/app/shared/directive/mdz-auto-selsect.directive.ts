b699d69dee2432a6122bb36aeedfeb85c14831f8
Zoran <vulezor@gmail.com>
2019-09-25 12:59:45 +0200

    chck

--- src/app/shared/directive/mdz-auto-selsect.directive.ts
+++ src/app/shared/directive/mdz-auto-selsect.directive.ts
@@ -2,23 +2,28 @@ import { Directive, HostListener } from '@angular/core';
 import { BehaviorSubject, Subject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { debounceTime, switchMap, takeUntil, skip } from 'rxjs/operators';
+
 @Directive({
   selector: '[appMdzAutoSelsect]'
 })
 export class MdzAutoSelsectDirective {
   public term$ = new BehaviorSubject<string>('');
   public results$ = this.term$.pipe(
-    this.autocomplete(1000, term => this.fetch(term))
+    this.autocomplete(200, term => this.fetch(term))
   );
   constructor(private httpClient: HttpClient) {
     console.log('auto complete directive is initiated');
+    this.results$.subscribe(e => {
+      console.log(e);
+    });
   }
   @HostListener('keyup', ['$event'])
   inputChanged(event) {
-    console.log(event);
+    this.term$.next(event.target.value);
   }
+
   private autocomplete(time, selector) {
-    return source$ =>
+    return (source$: any) =>
       source$.pipe(
         debounceTime(time),
         switchMap((...args: any[]) =>
